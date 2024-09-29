2b520546ba4a2b42811e6f1d088087241b732981
sagevik <rsagevik@gmail.com>
2024-09-29 21:48:55 +0200

    add patch namedscratchpads
    add two terminal scratchpads

--- config.def.h
+++ config.def.h
@@ -36,9 +36,11 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class      instance    title       		tags mask     isfloating   monitor    scratch key */
+	{ "Gimp",     NULL,       NULL,       		0,            1,           -1,        0  },
+	{ "firefox",  NULL,       NULL,       		1 << 8,       0,           -1,        0  },
+	{ NULL,       NULL,       "scratchpad",     	0,            1,           -1,       's' },
+	{ NULL,       NULL,       "scratchpad2",    	0,            1,           -1,       'a' },
 };
 
 /* layout(s) */
@@ -78,11 +80,18 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
+/*First arg only serves to match against key in rules*/
+static const char *scratchpadcmd[] = {"s", "st", "-t", "scratchpad", NULL}; 
+static const char *scratchpadcmd2[] = {"a", "st", "-t", "scratchpad2", NULL}; 
+
+
 #include "movestack.c"
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_s,  togglescratch,  {.v = scratchpadcmd } },
+	{ MODKEY,                       XK_a,  togglescratch,  {.v = scratchpadcmd2 } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
