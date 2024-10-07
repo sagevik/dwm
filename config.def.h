2a3cf6f4fb3ad6758f28a2c312013c20445966cf
sagevik <rsagevik@gmail.com>
2024-10-07 19:42:24 +0200

    add patch dynamicscratchpads

--- config.def.h
+++ config.def.h
@@ -92,52 +92,55 @@ static const char *scratchcmdB[] = {"s", "st", "-t", "ScratchB", NULL};
 
 #include "movestack.c"
 static const Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_a,      togglescratch,  {.v = scratchcmdA } },
-	{ MODKEY,                       XK_s,      togglescratch,  {.v = scratchcmdB } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY|ShiftMask,             XK_h,      setcfact,       {.f = +0.25} },
-	{ MODKEY|ShiftMask,             XK_l,      setcfact,       {.f = -0.25} },
-	{ MODKEY|ShiftMask,             XK_o,      setcfact,       {.f =  0.00} },
-	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
-	{ MODKEY|Mod1Mask,              XK_0,      togglegaps,     {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY,                       XK_q,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} }, // set layout 0 []= tile
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} }, // set layout 1 ><> floating
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} }, // set layout 2 [M] monocle
-	{ MODKEY|ShiftMask,             XK_t,      setlayout,      {.v = &layouts[3]} }, // set layout 3 [\\] dwindle
-	{ MODKEY|ControlMask,           XK_t,      setlayout,      {.v = &layouts[4]} }, // set layout 4 TTT bstack
-	{ MODKEY|ShiftMask,             XK_m,      setlayout,      {.v = &layouts[5]} }, // set layout 5 |M| centeredmaster
-	{ MODKEY|ControlMask,           XK_m,      setlayout,      {.v = &layouts[6]} }, // set layout 6 >M> centeredfloatingmaster
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,           {0} },
+	/* modifier                     key        function           argument */
+	{ MODKEY,                       XK_p,      spawn,             {.v = dmenucmd } },
+	{ MODKEY,                       XK_Return, spawn,             {.v = termcmd } },
+	{ MODKEY,                       XK_a,      togglescratch,     {.v = scratchcmdA } },
+	{ MODKEY,                       XK_s,      togglescratch,     {.v = scratchcmdB } },
+	{ MODKEY|ShiftMask,             XK_u,      scratchpad_show,   {0} },
+	{ MODKEY|ControlMask,           XK_u,      scratchpad_hide,   {0} },
+	{ MODKEY|Mod1Mask,              XK_u,      scratchpad_remove, {0} },
+	{ MODKEY,                       XK_b,      togglebar,         {0} },
+	{ MODKEY,                       XK_j,      focusstack,        {.i = +1 } },
+	{ MODKEY,                       XK_k,      focusstack,        {.i = -1 } },
+	{ MODKEY,                       XK_i,      incnmaster,        {.i = +1 } },
+	{ MODKEY,                       XK_d,      incnmaster,        {.i = -1 } },
+	{ MODKEY,                       XK_h,      setmfact,          {.f = -0.05} },
+	{ MODKEY,                       XK_l,      setmfact,          {.f = +0.05} },
+	{ MODKEY|ShiftMask,             XK_h,      setcfact,          {.f = +0.25} },
+	{ MODKEY|ShiftMask,             XK_l,      setcfact,          {.f = -0.25} },
+	{ MODKEY|ShiftMask,             XK_o,      setcfact,          {.f =  0.00} },
+	{ MODKEY|ShiftMask,             XK_j,      movestack,         {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,      movestack,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_Return, zoom,              {0} },
+	{ MODKEY|Mod1Mask,              XK_0,      togglegaps,        {0} },
+	{ MODKEY,                       XK_Tab,    view,              {0} },
+	{ MODKEY,                       XK_q,      killclient,        {0} },
+	{ MODKEY,                       XK_t,      setlayout,         {.v = &layouts[0]} }, // set layout 0 []= tile
+	{ MODKEY,                       XK_f,      setlayout,         {.v = &layouts[1]} }, // set layout 1 ><> floating
+	{ MODKEY,                       XK_m,      setlayout,         {.v = &layouts[2]} }, // set layout 2 [M] monocle
+	{ MODKEY|ShiftMask,             XK_t,      setlayout,         {.v = &layouts[3]} }, // set layout 3 [\\] dwindle
+	{ MODKEY|ControlMask,           XK_t,      setlayout,         {.v = &layouts[4]} }, // set layout 4 TTT bstack
+	{ MODKEY|ShiftMask,             XK_m,      setlayout,         {.v = &layouts[5]} }, // set layout 5 |M| centeredmaster
+	{ MODKEY|ControlMask,           XK_m,      setlayout,         {.v = &layouts[6]} }, // set layout 6 >M> centeredfloatingmaster
+	{ MODKEY,                       XK_space,  setlayout,         {0} },
+	{ MODKEY|ShiftMask,             XK_space,  togglefloating,    {0} },
+	{ MODKEY,                       XK_0,      view,              {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,      tag,               {.ui = ~0 } },
+	{ MODKEY,                       XK_comma,  focusmon,          {.i = -1 } },
+	{ MODKEY,                       XK_period, focusmon,          {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,  tagmon,            {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period, tagmon,            {.i = +1 } },
+	TAGKEYS(                        XK_1,                         0)
+	TAGKEYS(                        XK_2,                         1)
+	TAGKEYS(                        XK_3,                         2)
+	TAGKEYS(                        XK_4,                         3)
+	TAGKEYS(                        XK_5,                         4)
+	TAGKEYS(                        XK_6,                         5)
+	TAGKEYS(                        XK_7,                         6)
+	TAGKEYS(                        XK_8,                         7)
+	TAGKEYS(                        XK_9,                         8)
+	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,              {0} },
 };
 
 /* button definitions */
