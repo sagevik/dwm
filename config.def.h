aadf424ccc83f5fb0289c9bf43588cf542368643
sagevik <rsagevik@gmail.com>
2024-11-07 19:30:41 +0100

    deactivate smartgaps

--- config.def.h
+++ config.def.h
@@ -8,7 +8,7 @@ static const unsigned int gappih    = 4;       	/* horiz inner gap between windo
 static const unsigned int gappiv    = 4;       	/* vert inner gap between windows */
 static const unsigned int gappoh    = 4;       	/* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 4;       	/* vert outer gap between windows and screen edge */
-static       int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
+static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
@@ -52,7 +52,7 @@ static const Rule rules[] = {
 	{ NULL,        NULL,        "ScratchF",    	  0,            1,		1,          1,           0,        -1,      50,50,900,800,	2,		 'f' },
 	{ NULL,        NULL,        "ScratchV",    	  0,            1,		1,          1,           0,        -1,      50,50,600,500,	2,		 'v' },
 	{ NULL,        NULL,        "Event Tester",       0,            0,		0,          0,           1,        -1,      50,50,500,500,	2,		  0  }, /* xev */
-	{ NULL,        NULL,        "Unlock Keyring",     0,            1,		0,          0,           1,        -1,      50,50,500,500,	2,		  0  }, /* xev */
+	{ NULL,        NULL,        "Unlock Keyring",     0,            1,		0,          0,           1,        -1,      50,50,500,500,	2,		  0  },
 };
 
 /* window following */
