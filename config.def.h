bddc801d0367fb6b95b15bb7fabdb668581568ac
sagevik <rsagevik@gmail.com>
2024-11-16 00:35:23 +0100

    remove unused layouts

--- config.def.h
+++ config.def.h
@@ -73,19 +73,14 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },			// first entry is default
-	{ "><>",      NULL },			// no layout function means floating behavior
-	{ "[M]",      monocle },
 	{ "[\\]",     dwindle },
 	{ "TTT",      bstack },
+    { "###",      nrowgrid },
+    { "[M]",      monocle },
 	{ "|M|",      centeredmaster },
 	{ ">M>",      centeredfloatingmaster },
-	{ "[@]",      spiral },
-	{ "H[]",      deck },
-	{ "===",      bstackhoriz },
-	{ "HHH",      grid },
-	{ "###",      nrowgrid },
-	{ "---",      horizgrid },
-	{ ":::",      gaplessgrid },
+    { "===",      bstackhoriz },
+    { "><>",      NULL },			// no layout function means floating behavior
 	{ NULL,       NULL },
 };
 
@@ -146,20 +141,20 @@ static const Key keys[] = {
 	{ MODKEY|Mod1Mask,                      XK_0,      togglegaps,        {0} },
 	{ MODKEY,                               XK_Tab,    view,              {0} },
 	{ MODKEY,                               XK_q,      killclient,        {0} },
+// Set layouts
 	{ MODKEY,                               XK_t,      setlayout,         {.v = &layouts[0]} }, // set layout 0 []= tile
-	{ MODKEY,                               XK_f,      setlayout,         {.v = &layouts[1]} }, // set layout 1 ><> floating
-	{ MODKEY,                               XK_m,      setlayout,         {.v = &layouts[2]} }, // set layout 2 [M] monocle
-	{ MODKEY|ShiftMask,                     XK_t,      setlayout,         {.v = &layouts[3]} }, // set layout 3 [\\] dwindle
-	{ MODKEY|ControlMask,                   XK_t,      setlayout,         {.v = &layouts[4]} }, // set layout 4 TTT bstack
+    { MODKEY|ShiftMask,                     XK_t,      setlayout,         {.v = &layouts[1]} }, // set layout 1 [\\] dwindle
+    { MODKEY|ControlMask,                   XK_t,      setlayout,         {.v = &layouts[2]} }, // set layout 2 TTT bstack
+    { MODKEY|ShiftMask|ControlMask,         XK_t,      setlayout,         {.v = &layouts[3]} }, // set layout 3 ### nrowgrid
+	{ MODKEY,                               XK_m,      setlayout,         {.v = &layouts[4]} }, // set layout 4 [M] monocle
 	{ MODKEY|ShiftMask,                     XK_m,      setlayout,         {.v = &layouts[5]} }, // set layout 5 |M| centeredmaster
 	{ MODKEY|ControlMask,                   XK_m,      setlayout,         {.v = &layouts[6]} }, // set layout 6 >M> centeredfloatingmaster
-	{ MODKEY|Mod1Mask,                      XK_t,      setlayout,         {.v = &layouts[7]} }, // set layout 7 [@] spiral
-	{ MODKEY|ShiftMask|ControlMask,         XK_t,      setlayout,         {.v = &layouts[8]} }, // set layout 8 H[] deck
-	{ MODKEY|Mod1Mask,                      XK_m,      setlayout,         {.v = &layouts[9]} }, // set layout 9 === bstackhoriz
-	{ MODKEY,                               XK_g,      setlayout,         {.v = &layouts[10]} }, // set layout 10 HHH grid
-	{ MODKEY|ShiftMask,                     XK_g,      setlayout,         {.v = &layouts[11]} }, // set layout 11 ### nrowgrid
-	{ MODKEY|ControlMask,                   XK_g,      setlayout,         {.v = &layouts[12]} }, // set layout 12 --- horizgrid
-	{ MODKEY|Mod1Mask,                      XK_g,      setlayout,         {.v = &layouts[13]} }, // set layout 13 ::: gaplessgrid
+	{ MODKEY|ShiftMask|ControlMask,         XK_m,      setlayout,         {.v = &layouts[7]} }, // set layout 7 === bstackhoriz
+    { MODKEY,                               XK_f,      setlayout,         {.v = &layouts[8]} }, // set layout 8 ><> floating
+	//{ MODKEY,                               XK_g,      setlayout,         {.v = &layouts[10]} }, // set layout 10 HHH grid
+	//{ MODKEY|ShiftMask,                     XK_g,      setlayout,         {.v = &layouts[11]} }, // set layout 11 ### nrowgrid
+	//{ MODKEY|ControlMask,                   XK_g,      setlayout,         {.v = &layouts[12]} }, // set layout 12 --- horizgrid
+	//{ MODKEY|Mod1Mask,                      XK_g,      setlayout,         {.v = &layouts[13]} }, // set layout 13 ::: gaplessgrid
 	{ MODKEY,                               XK_space,  setlayout,         {0} },
 	{ MODKEY|ShiftMask,                     XK_space,  togglefloating,    {0} },
 	{ MODKEY,                               XK_0,      view,              {.ui = ~0 } },
--- vanitygaps.c
+++ vanitygaps.c
@@ -7,12 +7,9 @@ static void bstack(Monitor *m);
 static void bstackhoriz(Monitor *m);
 static void centeredmaster(Monitor *m);
 static void centeredfloatingmaster(Monitor *m);
-static void deck(Monitor *m);
 static void dwindle(Monitor *m);
 static void fibonacci(Monitor *m, int s);
-static void grid(Monitor *m);
 static void nrowgrid(Monitor *m);
-static void spiral(Monitor *m);
 static void tile(Monitor *m);
 /* Internals */
 static void getgaps(Monitor *m, int *oh, int *ov, int *ih, int *iv, unsigned int *nc);
@@ -350,51 +347,6 @@ centeredfloatingmaster(Monitor *m)
 }
 
 /*
- * Deck layout + gaps
- * https://dwm.suckless.org/patches/deck/
- */
-void
-deck(Monitor *m)
-{
-	unsigned int i, n;
-	int oh, ov, ih, iv;
-	int mx = 0, my = 0, mh = 0, mw = 0;
-	int sx = 0, sy = 0, sh = 0, sw = 0;
-	float mfacts, sfacts;
-	int mrest, srest;
-	Client *c;
-
-	getgaps(m, &oh, &ov, &ih, &iv, &n);
-	if (n == 0)
-		return;
-
-	sx = mx = m->wx + ov;
-	sy = my = m->wy + oh;
-	sh = mh = m->wh - 2*oh - ih * (MIN(n, m->nmaster) - 1);
-	sw = mw = m->ww - 2*ov;
-
-	if (m->nmaster && n > m->nmaster) {
-		sw = (mw - iv) * (1 - m->mfact);
-		mw = mw - iv - sw;
-		sx = mx + mw + iv;
-		sh = m->wh - 2*oh;
-	}
-
-	getfacts(m, mh, sh, &mfacts, &sfacts, &mrest, &srest);
-
-	if (n - m->nmaster > 0) /* override layout symbol */
-		snprintf(m->ltsymbol, sizeof m->ltsymbol, "D %d", n - m->nmaster);
-
-	for (i = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++)
-		if (i < m->nmaster) {
-			resize(c, mx, my, mw - (2*c->bw), mh * (c->cfact / mfacts) + (i < mrest ? 1 : 0) - (2*c->bw), 0);
-			my += HEIGHT(c) + ih;
-		} else {
-			resize(c, sx, sy, sw - (2*c->bw), sh - (2*c->bw), 0);
-		}
-}
-
-/*
  * Fibonacci layout + gaps
  * https://dwm.suckless.org/patches/fibonacci/
  */
@@ -489,164 +441,6 @@ void
 dwindle(Monitor *m)
 {
 	fibonacci(m, 1);
-}
-
-void
-spiral(Monitor *m)
-{
-	fibonacci(m, 0);
-}
-
-/*
- * Gappless grid layout + gaps (ironically)
- * https://dwm.suckless.org/patches/gaplessgrid/
- */
-void
-gaplessgrid(Monitor *m)
-{
-	unsigned int i, n;
-	int x, y, cols, rows, ch, cw, cn, rn, rrest, crest; // counters
-	int oh, ov, ih, iv;
-	Client *c;
-
-	getgaps(m, &oh, &ov, &ih, &iv, &n);
-	if (n == 0)
-		return;
-
-	/* grid dimensions */
-	for (cols = 0; cols <= n/2; cols++)
-		if (cols*cols >= n)
-			break;
-	if (n == 5) /* set layout against the general calculation: not 1:2:2, but 2:3 */
-		cols = 2;
-	rows = n/cols;
-	cn = rn = 0; // reset column no, row no, client count
-
-	ch = (m->wh - 2*oh - ih * (rows - 1)) / rows;
-	cw = (m->ww - 2*ov - iv * (cols - 1)) / cols;
-	rrest = (m->wh - 2*oh - ih * (rows - 1)) - ch * rows;
-	crest = (m->ww - 2*ov - iv * (cols - 1)) - cw * cols;
-	x = m->wx + ov;
-	y = m->wy + oh;
-
-	for (i = 0, c = nexttiled(m->clients); c; i++, c = nexttiled(c->next)) {
-		if (i/rows + 1 > cols - n%cols) {
-			rows = n/cols + 1;
-			ch = (m->wh - 2*oh - ih * (rows - 1)) / rows;
-			rrest = (m->wh - 2*oh - ih * (rows - 1)) - ch * rows;
-		}
-		resize(c,
-			x,
-			y + rn*(ch + ih) + MIN(rn, rrest),
-			cw + (cn < crest ? 1 : 0) - 2*c->bw,
-			ch + (rn < rrest ? 1 : 0) - 2*c->bw,
-			0);
-		rn++;
-		if (rn >= rows) {
-			rn = 0;
-			x += cw + ih + (cn < crest ? 1 : 0);
-			cn++;
-		}
-	}
-}
-
-/*
- * Gridmode layout + gaps
- * https://dwm.suckless.org/patches/gridmode/
- */
-void
-grid(Monitor *m)
-{
-	unsigned int i, n;
-	int cx, cy, cw, ch, cc, cr, chrest, cwrest, cols, rows;
-	int oh, ov, ih, iv;
-	Client *c;
-
-	getgaps(m, &oh, &ov, &ih, &iv, &n);
-
-	/* grid dimensions */
-	for (rows = 0; rows <= n/2; rows++)
-		if (rows*rows >= n)
-			break;
-	cols = (rows && (rows - 1) * rows >= n) ? rows - 1 : rows;
-
-	/* window geoms (cell height/width) */
-	ch = (m->wh - 2*oh - ih * (rows - 1)) / (rows ? rows : 1);
-	cw = (m->ww - 2*ov - iv * (cols - 1)) / (cols ? cols : 1);
-	chrest = (m->wh - 2*oh - ih * (rows - 1)) - ch * rows;
-	cwrest = (m->ww - 2*ov - iv * (cols - 1)) - cw * cols;
-	for (i = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++) {
-		cc = i / rows;
-		cr = i % rows;
-		cx = m->wx + ov + cc * (cw + iv) + MIN(cc, cwrest);
-		cy = m->wy + oh + cr * (ch + ih) + MIN(cr, chrest);
-		resize(c, cx, cy, cw + (cc < cwrest ? 1 : 0) - 2*c->bw, ch + (cr < chrest ? 1 : 0) - 2*c->bw, False);
-	}
-}
-
-/*
- * Horizontal grid layout + gaps
- * https://dwm.suckless.org/patches/horizgrid/
- */
-void
-horizgrid(Monitor *m) {
-	Client *c;
-	unsigned int n, i;
-	int oh, ov, ih, iv;
-	int mx = 0, my = 0, mh = 0, mw = 0;
-	int sx = 0, sy = 0, sh = 0, sw = 0;
-	int ntop, nbottom = 1;
-	float mfacts = 0, sfacts = 0;
-	int mrest, srest, mtotal = 0, stotal = 0;
-
-	/* Count windows */
-	getgaps(m, &oh, &ov, &ih, &iv, &n);
-	if (n == 0)
-		return;
-
-	if (n <= 2)
-		ntop = n;
-	else {
-		ntop = n / 2;
-		nbottom = n - ntop;
-	}
-	sx = mx = m->wx + ov;
-	sy = my = m->wy + oh;
-	sh = mh = m->wh - 2*oh;
-	sw = mw = m->ww - 2*ov;
-
-	if (n > ntop) {
-		sh = (mh - ih) / 2;
-		mh = mh - ih - sh;
-		sy = my + mh + ih;
-		mw = m->ww - 2*ov - iv * (ntop - 1);
-		sw = m->ww - 2*ov - iv * (nbottom - 1);
-	}
-
-	/* calculate facts */
-	for (i = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++)
-		if (i < ntop)
-			mfacts += c->cfact;
-		else
-			sfacts += c->cfact;
-
-	for (i = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++)
-		if (i < ntop)
-			mtotal += mh * (c->cfact / mfacts);
-		else
-			stotal += sw * (c->cfact / sfacts);
-
-	mrest = mh - mtotal;
-	srest = sw - stotal;
-
-	for (i = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++)
-		if (i < ntop) {
-			resize(c, mx, my, mw * (c->cfact / mfacts) + (i < mrest ? 1 : 0) - (2*c->bw), mh - (2*c->bw), 0);
-			mx += WIDTH(c) + iv;
-		} else {
-			resize(c, sx, sy, sw * (c->cfact / sfacts) + ((i - ntop) < srest ? 1 : 0) - (2*c->bw), sh - (2*c->bw), 0);
-			sx += WIDTH(c) + iv;
-		}
 }
 
 /*
