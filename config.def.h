d293e3c6903a09ce44c4bde3d32f19b29dd87a79
sagevik <rsagevik@gmail.com>
2025-01-23 23:52:24 +0100

    change colour

--- config.def.h
+++ config.def.h
@@ -23,7 +23,7 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#3b4252";
+static const char col_cyan[]        = "#005577";
 static const char col_red[]         = "#dd2f3a";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
